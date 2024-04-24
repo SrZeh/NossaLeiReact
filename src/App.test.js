5c11693320e09e0f1a19da63673948884a35f524
viniciusgdoliveira <viniciusgdoliveira@gmail.com>
2024-04-24 20:15:12 -0300

    update app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Pagina Login Nossa Lei', () => {
   render(<App />);
   const linkElement = screen.getByText(/Nossa Lei/i);
   expect(linkElement).toBeInTheDocument();
