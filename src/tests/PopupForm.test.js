c79f2f95e70ab459c10ce841eaab57f59fec1e95
SrZeh <josebiel@gmail.com>
2024-06-10 15:36:41 -0300

    alteracoes e testes funcionando

--- src/tests/PopupForm.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../components/PopupForm';
-
-test('renders Pagina Login Nossa Lei', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Leis/i);
-  expect(linkElement).toBeInTheDocument();
-});
