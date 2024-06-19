6bcce2f6949110c0de9bf6fc7bafde59f282b6aa
SrZeh <josebiel@gmail.com>
2024-06-19 19:00:31 -0300

    haha

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,13 @@
-// import globals from "globals";
-// import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-// import { fixupConfigRules } from "@eslint/compat";
-
-
-// export default [
-//   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-//   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-//   {languageOptions: { globals: globals.browser }},
-//   ...fixupConfigRules(pluginReactConfig),
-// ];
-
 import globals from "globals";
+import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
+
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  { languageOptions: { globals: globals.browser } },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
-  {
-    settings: {
-      react: {
-        version: 'detect', // 'detect' irá automaticamente pegar a versão do React instalada
-      },
-    },
-  },
 ];
