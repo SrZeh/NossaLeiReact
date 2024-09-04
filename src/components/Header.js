48e6690ca9bdd353859cd1cb4e7f86e2e6e3aa1d
SrZeh <josebiel@gmail.com>
2024-09-04 19:28:41 -0300

    logo e respiro

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,63 @@
+// import React from 'react';
+// import homeLogo from '../assets/images/home.png';
+// import { Link, useNavigate } from "react-router-dom";
+// import { useAuth } from '../context/AuthContext';
+
+// const Header = () => {
+//     const { currentUser, logout } = useAuth();
+//     const navigate = useNavigate(); // useNavigate para redirecionamento
+
+//     const handleLogout = async () => {
+//         try {
+//             await logout(); // Executa a função de logout
+//             navigate("/"); // Redireciona para a página inicial
+//         } catch (error) {
+//             console.error("Failed to log out", error);
+//         }
+//     };
+
+//     return (
+//         <header className="header">
+//             <div className="logo">
+//                 <Link to="/law-list">
+//                     <img src={homeLogo} alt="Logo" />
+//                 </Link>
+//             </div>
+//             <nav>
+                
+//                 {/* <Link to="/">Home</Link> */}
+//                 <Link to="/law-list">Propostas de Leis</Link>
+//                 <Link to="/new-law">Crie Sua Lei</Link>
+//                 <Link to="/news">Notícias</Link>
+                
+//                 {currentUser ? (
+//                     <>
+//                         <span>Bem-vindo, {currentUser.email}</span>
+//                         <button onClick={handleLogout} style={{ cursor: 'pointer', margin: 20 }}>Logout</button>
+//                     </>
+//                 ) : (
+//                     <Link to="/new-user">Cadastro</Link>
+//                 )}
+//             </nav>
+//         </header>
+//     );
+// };
+
+// export default Header;
+
+// export function HeaderOnBoarding() {
+//     return (
+//         <header className="header">
+//             <div className="logo">
+//                 <Link to="/">
+//                     <img src={homeLogo} alt="Logo" />
+//                 </Link>
+//             </div>
+//         </header>
+//     );
+// }
+
+
 import React from 'react';
 import homeLogo from '../assets/images/home.png';
 import { Link, useNavigate } from "react-router-dom";
@@ -23,16 +83,14 @@ const Header = () => {
                     <img src={homeLogo} alt="Logo" />
                 </Link>
             </div>
-            <nav>
-                {/* <Link to="/">Home</Link> */}
+            <nav className="nav-links">
                 <Link to="/law-list">Propostas de Leis</Link>
                 <Link to="/new-law">Crie Sua Lei</Link>
                 <Link to="/news">Notícias</Link>
-
                 {currentUser ? (
                     <>
                         <span>Bem-vindo, {currentUser.email}</span>
-                        <button onClick={handleLogout} style={{ cursor: 'pointer' }}>Logout</button>
+                        <button onClick={handleLogout} style={{ cursor: 'pointer', margin: 20 }}>Logout</button>
                     </>
                 ) : (
                     <Link to="/new-user">Cadastro</Link>
